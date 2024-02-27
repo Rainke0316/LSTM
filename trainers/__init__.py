21686d0b50297649683ae209fd01537b38c580a4
Rainke0316 <1810492376@qq.com>
2024-02-27 19:00:38 +0800

    clone the structure

---
+++ trainers/__init__.py
@@ -0,0 +1,11 @@
+from .abs import AbstractTrainer
+from .IterMultiStepForecastTrainer import IterMultiStepForecastTrainer
+from .SAETrainer import SAETrainer
+from .SoftSensorTrainer import SoftSensorTrainer
+
+__all__ = [
+    "AbstractTrainer",
+    "IterMultiStepForecastTrainer",
+    "SAETrainer",
+    "SoftSensorTrainer",
+]
